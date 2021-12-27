b357bd579dff74905a619aa84de6bef0aef4bfa1
kimanicharles911 <kimanicharles911@gmail.com>
2021-12-27 16:13:03 +0300

    complete favourites logic functionality

--- frontend/src/components/FavouritesComponent.jsx
+++ frontend/src/components/FavouritesComponent.jsx
@@ -1,26 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
-import {useState} from 'react';
 
 const FavouritesComponent = ({favouritesProp, setFavouritesProp}) => {
 
-  const [spin, setSpin] = useState(false);
-  const likeUnlikeFunc = () => {
-
+  const unlikeFunc = (dataIndex) => {
+    const newFavouritesArr = favouritesProp.filter(favourite => favourite !== favouritesProp[dataIndex]);
+    setFavouritesProp(newFavouritesArr);
   };
 
   return(
     <main className="container fluid text-center">
       <section className="row justify-content-md-center mt-3 mb-3 gy-3">
-        {favouritesProp.length > 1 ?
+        {favouritesProp.length > 0 ?
           favouritesProp.map((dataPoint, index) => {
             return (
               <div className="col-sm-6" key={index}>
                 <div className="card mb-3" style={{ maxWidth: "540px" }}>
                   <div className="row g-0">
                     <div className="col-md-4">
-                      <img src={dataPoint.artworkUrl100} alt="spurgeon's art potrait" className="img-fluid rounded-start" />
+                      <img src={dataPoint.artworkUrl100} alt="media art image" className="img-fluid rounded-start" />
                     </div>
                     <div className="col-md-8">
                       <div className="card-body">
@@ -31,7 +29,7 @@ const FavouritesComponent = ({favouritesProp, setFavouritesProp}) => {
                         <br/>
                         <p className="card-text"><small className="text-muted">Kind: {dataPoint.kind}</small></p>
                         <span>&nbsp;|&nbsp;</span>
-                        <p className="card-text"><small className="text-muted small-like-tag" onClick={() => likeUnlikeFunc(index) }>{dataPoint.likeStatus ?  'Unlike' : 'Like'}<a href="#" type="button" className="nav-link like-icons"><FontAwesomeIcon icon={faHeartRegular} className="font-awesome-icon-component" /></a></small></p>
+                        <p className="card-text"><small className="text-muted small-like-tag" onClick={() => unlikeFunc(index) }>{dataPoint.likeStatus ?  'Unlike' : 'Like'}<a type="button" className="nav-link like-icons"><FontAwesomeIcon icon={faHeart} className="font-awesome-icon-component" /></a></small></p>
                         <p className="card-text">Description: {dataPoint.description ? dataPoint.description : null } {dataPoint.description === undefined && dataPoint.shortDescription ? dataPoint.shortDescription : "Not available." } </p>
                         <br />
                         <p className="card-text">Long Description: {dataPoint.longDescription ? dataPoint.longDescription : "Not available." }</p>
@@ -42,12 +40,7 @@ const FavouritesComponent = ({favouritesProp, setFavouritesProp}) => {
               </div>
             )
           })
-          :
-          spin ? 
-            <div className="spinner-border" id="spinner-border" role="status">
-              <span className="visually-hidden">Loading..</span>
-            </div>
-            : "Search results will appear here"
+          : "You have not liked any media so far. Please start by liking and come back here to view them as your favourites."
         }
       </section>
     </main>
--- frontend/src/components/MainComponent.jsx
+++ frontend/src/components/MainComponent.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 import './MainComponent.css';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 const MainComponent = ({favouritesProp, setFavouritesProp}) => {
 
@@ -93,9 +93,6 @@ const MainComponent = ({favouritesProp, setFavouritesProp}) => {
       apiData[dataIndex].likeStatus = true;
     }
   };
-  useEffect(() => {
-    console.log(`favouritesProp`, favouritesProp);
-  }, [favouritesProp])
   // https://itunes.apple.com/search?term=jack+johnson&media=movie
   // jack johnson
 
@@ -139,20 +136,18 @@ const MainComponent = ({favouritesProp, setFavouritesProp}) => {
           <label className={filterBtnStyling}  htmlFor="btnradioEbook" onClick={() => {filterBtnClickHandler("ebook")}}>ebook</label>
           <input type="radio" className="btn-check" name="btnradio" id="btnradioAll"/>
           <label className={filterBtnStyling}  htmlFor="btnradioAll" onClick={() => {filterBtnClickHandler("all")}}>All</label>
-          <input type="radio" className="btn-check" name="btnradio" id="btnradioTrial"/>
-          <label className={filterBtnStyling}  htmlFor="btnradioTrial" onClick={() => {}}>Trial</label>
         </div>
       </section>
       <hr />
       <section className="row justify-content-md-center mt-3 mb-3 gy-3">
-        {apiData.length > 1 ?
+        {apiData.length > 0 ?
           apiData.map((dataPoint, index) => {
             return (
               <div className="col-sm-6" key={index}>
                 <div className="card mb-3" style={{ maxWidth: "540px" }}>
                   <div className="row g-0">
                     <div className="col-md-4">
-                      <img src={dataPoint.artworkUrl100} alt="spurgeon's art potrait" className="img-fluid rounded-start" />
+                      <img src={dataPoint.artworkUrl100} alt="media art image" className="img-fluid rounded-start" />
                     </div>
                     <div className="col-md-8">
                       <div className="card-body">
@@ -163,7 +158,7 @@ const MainComponent = ({favouritesProp, setFavouritesProp}) => {
                         <br/>
                         <p className="card-text"><small className="text-muted">Kind: {dataPoint.kind}</small></p>
                         <span>&nbsp;|&nbsp;</span>
-                        <p className="card-text"><small className="text-muted small-like-tag" onClick={() => likeUnlikeFunc(index) }>{dataPoint.likeStatus ?  'Unlike' : 'Like'}<a href="#" type="button" className="nav-link like-icons"><FontAwesomeIcon icon={faHeartRegular} className="font-awesome-icon-component" /></a></small></p>
+                        <p className="card-text"><small className="text-muted small-like-tag" onClick={() => likeUnlikeFunc(index) }>{dataPoint.likeStatus ?  'Unlike' : 'Like'}<a type="button" className="nav-link like-icons"><FontAwesomeIcon icon={dataPoint.likeStatus ? faHeart :faHeartRegular} className="font-awesome-icon-component" /></a></small></p>
                         <p className="card-text">Description: {dataPoint.description ? dataPoint.description : null } {dataPoint.description === undefined && dataPoint.shortDescription ? dataPoint.shortDescription : "Not available." } </p>
                         <br />
                         <p className="card-text">Long Description: {dataPoint.longDescription ? dataPoint.longDescription : "Not available." }</p>
