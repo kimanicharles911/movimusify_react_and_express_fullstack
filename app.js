4fb1cae0e3b47dec3c14472fcca552db5dab62b2
kimanicharles911 <kimanicharles911@gmail.com>
2022-01-02 14:25:41 +0300

    add backend comments

--- app.js
+++ app.js
@@ -15,9 +15,18 @@ app.use(
   })
 )
 
+/* 
+  * I defined a GET route that utilises an external 3rd party API from itunes
+  * The API url is stored in the URL constant variable.
+  * The API's URL utilises the term and media values received from the called GET request query object.
+  * I used axios to fetch the API data from itunes with appropriate error handling.
+  Example test URLS:
+  1. https://itunes.apple.com/search?term=jack+johnson&media=music
+  2. https://itunes.apple.com/search?term=jack+johnson
+*/
 app.get('/search', (req, res) => {
-  const urlPrefix = `https://itunes.apple.com/search?term=${req.query.term}&media=${req.query.media ? req.query.media : ''}`;
-  axios.get(urlPrefix)
+  const URL = `https://itunes.apple.com/search?term=${req.query.term}&media=${req.query.media ? req.query.media : ''}`;
+  axios.get(URL)
     .then((response) => {
       return res.json(response.data);
     }).catch(err => {
@@ -25,6 +34,12 @@ app.get('/search', (req, res) => {
     })
 })
 
+/* 
+  * I created a function that enables serving of static files from the frontend.
+  * I begun with a condition that ensures that the operating environment is production.
+  * I then set the static folder location.
+  * I then set the route to all other possible routes so as to load the frontend root file which in react is a index.html file.
+*/
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
@@ -35,3 +50,22 @@ if(process.env.NODE_ENV === 'production'){
 app.listen(port.EXPRESS_APP_PORT, () => {
   console.log(`App server listening at http://localhost:${port.EXPRESS_APP_PORT}`)
 })
+
+/* 
+ * I first imported the express module.
+  * I then stored the called express function in a variable app.
+  * I stored the EXPRESS_APP_PORT module from the config folder in a variable called port.
+  * I executed the .json() middleware function on the express app function to enable sending of data in JSON format.
+  * I imported the axios http client for making fetch requests.
+  * I imported the helmet library which improves the security of my App.
+  * I then executed the helmet middleware function on the the express ap function.
+  * From the app object with the express function I used the listen method and set the port.
+  * I then added a get request that returns a fail message for any request to an undefined route.
+*/
+
+/* 
+REFERENCES
+==========>
+* Learnt to deploy a fullstack application from: https://youtu.be/71wSzpLyW9k
+* Learnt to debug helmet deployment issue on contentSecurityPolicy from: https://helmetjs.github.io/
+*/
--- test/Async.test.js
+++ test/Async.test.js
@@ -1,3 +1,7 @@
+/* 
+  * I imported the axios http client for making fetch requests.
+  * I imported the expect library from chai used for test assertions.
+*/
 const axios = require('axios');
 const expect = require('chai').expect;
 
@@ -10,3 +14,13 @@ it('Axios get request test', () => {
       expect(typeof response).to.equal('object');
     })
 }).timeout(10000);
+
+/* 
+  * I wrote a test function that tests whether the API is fetched as expected.
+  * I utilised axios which fetches the API data from itunes.
+  * I then create two assertions on the API data received
+    1. That the first element of the data array must have artistName as one of it's property.
+    2. The length of the data array received should be 50.
+  * I also wrote a test assertion on the response that makes sure the response is of the object type.
+  * I increased the default mocha timeout by setting it to 10 seconds which makes sure the API will receive a response by that time.
+*/
